fb88d19819db1670806268becd8a7af99c975333
Douglas Belarmino <douglas.belarr@gmail.com>
2020-08-20 12:48:23 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+
+import express from "express";
+
+const routes = express.Router();
+
+routes.get("/", (req, res) => {
+  return res.json({ ok: true });
+});
+
+export default routes;
